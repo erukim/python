ab998441ac27cd00662311fe006011364681f32a
erukim <erukim@hiplay.kr>
2023-04-18 23:55:14 +0900

    ㅇㅇ

--- CHAPTER 1 연습문제.py
+++ CHAPTER 1 연습문제.py
@@ -5,3 +5,84 @@
 
 # 1.2
 # 객체지향 프로그래밍, 쉽게 구현할수 있는 API, 안정적인 메모리 관리
+
+# 1.3
+# 100
+# 300
+# 100 + 200
+# 100 200
+# 100 200
+# 100200
+# Hello Python!
+# Hello Python !
+# HelloPython!
+# HelloPython!
+# **********
+# **********
+
+# 1.4
+# Hello Python!
+# Hello Python!
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# 1.5
+# for i in range(5):
+#     print('Welcom to Pyton!!')
+
+# 1.6
+# name = str(input('이름을 입력하세요. : '))
+# scl = str(input('학교를 입력하세요. : '))
+# print('*' * 35)
+# print('안녕하세요~')
+# print('저는', name, '입니다.')
+# print(scl, '정보통신학과', '1학년입니다')
+# print('*' * 35)
+
+# 1.7
+# num_lines = 4
+# for i in range(num_lines):
+#     num_stars = i * 2 + 1
+#     line = '*' * num_stars
+#     print(line.center(num_lines * 2 - 1))
+
+# 1.8
+# lines = ["*******", "*****", "***", "*"]
+# max_width = len(lines[0])
+# for line in lines:
+#     padding = (max_width - len(line)) // 2
+#     print(" " * padding + line + " " * padding)
+
+# 1.9
+# 200
+# 4005
+# 4.0
+# 27
+# 729
+# 3.0
+# 3
+# 0
+
+# 1.10
+# (1) 55
+# (2) 31.4
+# (3) 100
+# (4) 625
+# (5) 80
+# (6) 300
+
+# 1.11
+# 120.0
+
+# 1.12
+# 95.0
+
+# 1.13
+# 499.00520375000003
+# 8.316753395833334
+
+# 1.14
+# (1) 3! = 3 * 2 * 1 = 6
+# (2) 5! = 5 * 4 * 3 * 2 * 1 = 120
+# (3) 12! = 12 * 11 * 10 * ... * 2 * 1 = 479001600
+# (4) 20! = 20 * 19 * 18 * ... * 2 * 1 = 2432902008176640000
---
+++ CHAPTER 3 연습문제.py
@@ -0,0 +1,7 @@
+# 3.3
+# age = int(input('나이를 입력하시오 : '))
+# height = int(input('키를 입력하시오 : '))
+# if age >= 19 and height >= 150:
+#     print('입장할 수 있습니다.')
+# else:
+#     print('입장할 수 없습니다.')
