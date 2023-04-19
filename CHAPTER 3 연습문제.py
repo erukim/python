a3dd190890e55caa25e5882977f58f8ed8e07270
erukim <erukim@hiplay.kr>
2023-04-19 22:03:52 +0900

    3.8

--- CHAPTER 3 연습문제.py
+++ CHAPTER 3 연습문제.py
@@ -15,3 +15,14 @@
 #     small_num = num2
 #     big_num = num1
 # print(small_num, big_num)
+
+# 3.8
+# x, y = map(int, input("점의 좌표 x, y를 입력하시오 : ").split())
+# if x > 0 and y > 0:
+#     print("1사분면에 있음")
+# elif x < 0 and y > 0:
+#     print("2사분면에 있음")
+# elif x < 0 and y < 0:
+#     print("3사분면에 있음")
+# else:
+#     print("4사분면에 있음")
