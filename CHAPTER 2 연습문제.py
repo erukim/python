16177d53164bf50ef6f638b66d3d0b13514027da
erukim <erukim@hiplay.kr>
2023-04-19 21:37:27 +0900

    2.11

--- CHAPTER 2 연습문제.py
+++ CHAPTER 2 연습문제.py
@@ -46,3 +46,8 @@
 # celsius = int(input('섭씨온도를 입력하세요 : '))
 # fahrenheit = (9/5) * celsius + 32
 # print(f'섭씨 {celsius} 도는 화씨 {fahrenheit} 도 입니다.')
+
+# 2.11
+fahrenheit = int(input("화씨 온도를 입력하세요: "))
+celsius = (fahrenheit - 32) * 5/9
+print("화씨", fahrenheit, '도는 섭씨', round(celsius,2), "도 입니다.")
