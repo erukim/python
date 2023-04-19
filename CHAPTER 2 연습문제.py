209c0eae940843ed16ef0712bbd94e33cbacd8f6
erukim <erukim@hiplay.kr>
2023-04-19 21:10:14 +0900

    챕2 백업 1

---
+++ CHAPTER 2 연습문제.py
@@ -0,0 +1,10 @@
+# 2.1
+# print(200,'+',300,'+',400,'=',900)
+
+# 2.2
+# width = 30
+# height = 60
+# print(width)
+# print(height)
+
+# 2.3
--- CHAPTER 3 연습문제.py
+++ CHAPTER 3 연습문제.py
@@ -5,3 +5,13 @@
 #     print('입장할 수 있습니다.')
 # else:
 #     print('입장할 수 없습니다.')
+
+# 3.5
+# num1, num2 = map(int, input("두 정수를 입력하시오 : ").split())
+# if num1 < num2:
+#     small_num = num1
+#     big_num = num2
+# else:
+#     small_num = num2
+#     big_num = num1
+# print(small_num, big_num)
