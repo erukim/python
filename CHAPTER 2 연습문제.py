19a24a153d6852a49a60c955c6293b17713048a7
erukim <erukim@hiplay.kr>
2023-04-19 21:42:31 +0900

    2.13

--- CHAPTER 2 연습문제.py
+++ CHAPTER 2 연습문제.py
@@ -59,4 +59,9 @@
 # area = PI * radius ** 2
 # print("원의 반지름 =", radius, ", 원의 둘레 =", circumference, ", 원의 면적 =", area)
 
-
+# 2.13
+# PI = 3.141592
+# radius = float(input("원의 반지름을 입력하세요 : "))
+# circumference = 2 * PI * radius
+# area = PI * radius ** 2
+# print("원의 둘레 = ", circumference, ", 원의 면적 = ", area)
