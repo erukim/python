888347c3ff51b67bdfcdba09834cd394c7ca8795
erukim <erukim@hiplay.kr>
2023-04-20 15:06:04 +0900

    3.9

--- CHAPTER 3 연습문제.py
+++ CHAPTER 3 연습문제.py
@@ -26,3 +26,26 @@
 #     print("3사분면에 있음")
 # else:
 #     print("4사분면에 있음")
+
+# 3.9
+# num = int(input("정수를 입력하시오: "))
+# if num % 2 == 0:
+#     div2 = True
+# else:
+#     div2 = False
+# if num % 3 == 0:
+#     div3 = True
+# else:
+#     div3 = False
+# if div2:
+#     print(f"{num}는(은) 2(으)로 나누어집니다.")
+# else:
+#     print(f"{num}는(은) 2(으)로 나누어지지 않습니다.")
+# if div3:
+#     print(f"{num}는(은) 3(으)로 나누어집니다.")
+# else:
+#     print(f"{num}는(은) 3(으)로 나누어지지 않습니다.")
+# if div2 and div3:
+#     print(f"{num}는(은) 2와(과) 3 모두로 나누어집니다.")
+# else:
+#     print(f"{num}는(은) 2와(과) 3 모두로 나누어지지 않습니다.")
